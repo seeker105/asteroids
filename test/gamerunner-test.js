b6802b9e60ea7bc616fefc05ebdca703d5270be2
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-28 18:11:00 -0600

    add gamerunner test

---
+++ test/gamerunner-test.js
@@ -0,0 +1,32 @@
+const assert = require('chai').assert;
+const Ship = require('../lib/ship');
+const GameRunner = require('../lib/game_runner');
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+
+describe('GameRunner', function(){
+  it('creates game with ship attribute', function(){
+  var ship = new Ship();
+  var game = new GameRunner(ship);
+
+  assert.equal(game.ship, ship)
+  });
+
+  it('starts game', function(){
+    var ship = new Ship();
+    var game = new GameRunner(ship);
+    game.playGame(ship);
+
+    assert.equal(game.ship.x, 300)
+    assert.equal(game.ship.y, 300)
+  });
+
+  it('ends game', function(){
+    var ship = new Ship();
+    var game = new GameRunner(ship);
+    game.gameOver(ship);
+
+    assert.equal(game.ship.x, 1000)
+    assert.equal(game.ship.y, 1000)
+  });
+});
--- test/index.js
+++ test/index.js
@@ -2,3 +2,4 @@ require('./ship-test.js');
 require('./collisions-test.js');
 require('./asteroid-test.js');
 require('./asteroids-test.js');
+require('./gamerunner-test.js');
